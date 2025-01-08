acdadf38356869597fe3282cb8c6372af08520d9
zhangchuy <138084021+zhangchuy@users.noreply.github.com>
2025-01-08 09:49:34 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='STCAT',
-    version='1.0.2',
+    version='1.0.6',
     packages=setuptools.find_packages(),
     install_requires=get_requirements(),
     include_package_data=True,
